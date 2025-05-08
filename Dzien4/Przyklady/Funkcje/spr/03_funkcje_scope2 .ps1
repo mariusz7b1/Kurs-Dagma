50942acf59081a5f9a24593dde88e5b9f627b6c9
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2025-05-08 07:15:17 +0200

    Start4

---
+++ Dzien4/Przyklady/Funkcje/spr/03_funkcje_scope2 .ps1
@@ -0,0 +1,27 @@
+﻿Clear-Host
+Function test1()
+{
+    $G1++
+    $G2++
+    $G3=10
+    Write-Host "jestesm w funkcji 'test1' G1=$G1, G2=$G2, G3=$G3"
+
+  }
+
+
+Function test2()
+{
+    $G1++
+    $script:G2++
+    Write-Host "jestesm w funkcji 'test2' G1=$G1, G2=$G2, G3=$G3"
+}
+
+
+
+$G1=10
+$G2=20
+test1
+Write-Host "jestesm w Skrypcie        G1=$G1, G2=$G2, G3=$G3"
+$G3=30
+test2
+Write-Host "jestesm w Skrypcie        G1=$G1, G2=$G2, G3=$G3"
---
+++ Dzien4/Przyklady/Funkcje/spr/funkcje1.ps1
@@ -0,0 +1,31 @@
+﻿Function szukaj_dir()
+{
+$sciezka=$args[0]
+[int] $zm1=0
+
+$dir=Get-ChildItem -LiteralPath $sciezka
+foreach($zm in $dir)
+{
+    
+    $zm.BaseName+" " +$zm.mode+$zm.Length
+    if($zm.mode -match "d")
+    {
+        $zm2=szukaj_dir $zm.FullName
+        
+        
+        $zm1+=$zm2
+     
+    } 
+    else 
+    {
+        $zm1++
+    }
+}
+write-host $zm1.GetType()
+return $zm1
+}
+
+Clear-Host
+$liczbaPlikow=szukaj_dir "d:\dane"
+
+$liczbaPlikow
